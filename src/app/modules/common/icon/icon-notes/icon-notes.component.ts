92aeaef661a1a16bb4a224a29142730a45d7ddb9
Adil Shaikh <adil@invideo.io>
2020-10-10 17:42:58 +0530

    feat: deleted notes listing

---
+++ src/app/modules/common/icon/icon-notes/icon-notes.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+
+@Component({
+  selector: 'app-icon-notes',
+  templateUrl: './icon-notes.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IconNotesComponent {
+}
