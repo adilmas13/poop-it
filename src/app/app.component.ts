62d26552a53dab197631258d8851b3664ea5c105
Adil Shaikh <adil@invideo.io>
2020-10-10 19:39:12 +0530

    feat: animation for full screen toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
 import { AppStore } from './app-store.service'
+import { animate, style, transition, trigger } from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  changeDetection : ChangeDetectionStrategy.OnPush,
-  providers: [AppStore]
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [AppStore],
+  animations: [
+    trigger('inOut', [
+      transition(':enter', [
+        style({width: 0}),
+        animate('0.3s ease-out', style({width: '*'}))
+      ]),
+      transition(':leave', [
+        style({width: '*', opacity: 1}),
+        animate('0.3s ease-out', style({width: 0, opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MenuModule } from './modules/menu/menu.module';
-import { ComposerModule } from './modules/composer/composer.module';
-import { NotesListModule } from './modules/notes-list/notes-list.module';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { MenuModule } from './modules/menu/menu.module'
+import { ComposerModule } from './modules/composer/composer.module'
+import { NotesListModule } from './modules/notes-list/notes-list.module'
 import { IconModule } from './modules/common/icon/icon.module'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        MenuModule,
-        ComposerModule,
-        NotesListModule,
-        IconModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MenuModule,
+    ComposerModule,
+    NotesListModule,
+    IconModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
