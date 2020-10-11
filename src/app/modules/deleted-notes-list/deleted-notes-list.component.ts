803e46c3e96c5f3711adcf0721d969512fc0cf66
Adil Shaikh <adil@invideo.io>
2020-10-12 01:58:37 +0530

    fix: animation in delete listing

--- src/app/modules/deleted-notes-list/deleted-notes-list.component.ts
+++ src/app/modules/deleted-notes-list/deleted-notes-list.component.ts
@@ -3,13 +3,14 @@ import { AppStore } from '../../app-store.service'
 import { Note } from '../../models/note'
 import { noteCardAnimation } from '../../animations/note-card-animation'
 import { BehaviorSubject } from 'rxjs'
+import { scaleInOutAnimation } from '../../animations/scale-in-out-animation'
 
 @Component({
   selector: 'app-deleted-notes-list',
   templateUrl: './deleted-notes-list.component.html',
   styleUrls: ['./deleted-notes-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [noteCardAnimation]
+  animations: [noteCardAnimation, scaleInOutAnimation]
 })
 export class DeletedNotesListComponent implements AfterViewInit {
 
