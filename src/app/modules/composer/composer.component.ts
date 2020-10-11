d0a39eec33a3f49e214ace0c8750f4bb9f11350c
Adil Shaikh <adil@invideo.io>
2020-10-11 23:34:15 +0530

    feat: added unable to edit message in composer under delete mode

--- src/app/modules/composer/composer.component.ts
+++ src/app/modules/composer/composer.component.ts
@@ -11,11 +11,21 @@ import { animate, style, transition, trigger } from '@angular/animations'
     trigger('scaleDown', [
       transition(':enter', [
         style({opacity: 0, transform: 'scale(0)'}),
-        animate('0.3s ease-out', style({ opacity: 1, transform: 'scale(1)'}))
+        animate('0.3s ease-out', style({opacity: 1, transform: 'scale(1)'}))
       ]),
       transition(':leave', [
         style({opacity: 1}),
-        animate('0.3s ease-out', style({ opacity: 0, transform: 'scale(0)'}))
+        animate('0.3s ease-out', style({opacity: 0, transform: 'scale(0)'}))
+      ])
+    ]),
+    trigger('heightAnim', [
+      transition(':enter', [
+        style({height: 0, opacity: 0}),
+        animate('0.3s ease-out', style({height: '*', opacity: 1}))
+      ]),
+      transition(':leave', [
+        style({height: '*', opacity: 1}),
+        animate('0.3s ease-out', style({height: 0, opacity: 0}))
       ])
     ]),
   ]
