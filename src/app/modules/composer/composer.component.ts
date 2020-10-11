3103ce0d43b762e49ad37730c8682b0ae01db0fe
Adil Shaikh <adil@invideo.io>
2020-10-11 21:47:47 +0530

    feat: hiding lock button in delete mode

--- src/app/modules/composer/composer.component.ts
+++ src/app/modules/composer/composer.component.ts
@@ -1,11 +1,24 @@
 import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core'
 import { AppStore } from '../../app-store.service'
+import { animate, style, transition, trigger } from '@angular/animations'
 
 @Component({
   selector: 'app-composer',
   templateUrl: './composer.component.html',
   styleUrls: ['./composer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('scaleDown', [
+      transition(':enter', [
+        style({opacity: 0, transform: 'scale(0)'}),
+        animate('0.3s ease-out', style({ opacity: 1, transform: 'scale(1)'}))
+      ]),
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('0.3s ease-out', style({ opacity: 0, transform: 'scale(0)'}))
+      ])
+    ]),
+  ]
 })
 export class ComposerComponent implements OnDestroy {
 
